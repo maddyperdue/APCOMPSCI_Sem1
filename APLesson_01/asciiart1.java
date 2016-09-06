75004c680d6a6457b63ce85f40d8682c1381a992
maddyperdue <mperdue.sd@gmail.com>
2016-09-06 08:48:54 -0700

    Added Files

---
+++ APLesson_01/asciiart1.java
@@ -0,0 +1,11 @@
+public class asciiart1
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" ,--./,-.");
+		System.out.println("/,-._.--~\\");
+		System.out.println(" __}  {");
+		System.out.println("\\`-._,-`-,");
+		System.out.println(" `._,._,\'");
+	}
+}

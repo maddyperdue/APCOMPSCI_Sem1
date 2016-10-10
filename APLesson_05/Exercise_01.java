7511ad7b0d43126d021dd77ef903cf99fb175903
maddyperdue <mperdue.sd@gmail.com>
2016-10-10 09:17:31 -0700

    changes

---
+++ APLesson_05/Exercise_01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public  class Exercise_01
+{
+	public static void main(String[]args)
+	{
+		int player = (Math.random()*7)+1;
+		int computer = (Math.random()*7)+1;
+	
+		String winner = //output from diceRoll
+		System.out.println("You rolled a" + player);
+		System.out.println("Computer rolled a " + computer);
+		System.out.println("And the winner is " + winner);
+	}
+	
+	public static double diceRoll(int player, int computer)
+	{
+		String winner = "";
+		//if player is greater
+			//set winner to player
+		//if computer is greater
+			//set winner to computer
+		return winner;
+	}
+}

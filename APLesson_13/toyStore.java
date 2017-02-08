42998b61c49609c1025f7de53b7c743002cc2c26
maddyperdue <mperdue.sd@gmail.com>
2017-02-08 09:17:18 -0800

    i am sitting in the front row now

---
+++ APLesson_13/toyStore.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+public class toyStore
+{
+	public static void main(String[]args)
+	{
+		ArrayList<toy> toyList = new ArrayList<toy>();
+	}
+
+	public toyStore()
+	{
+		loadToys();
+	}
+	
+	public toyStore(ArrayList<toy>)
+	{
+		loadToys(toy);
+	}
+	
+	public String loadToys(String ts)
+	{
+		
+	}
+	
+	public String getThatToy(String nm)
+	{
+		
+	}
+	
+	public String getMostFrequentToy()
+	{
+		
+	}
+	
+	public String getMostFrequentType()
+	{
+		new int cars = 0;
+		new int figures = 0;
+		
+		for //each toy in toylist
+		if(car
+			cars++;
+		if(AF
+			figures++;
+	}
+	
+	public String toString()
+	{
+		return toyList;
+	}
+}
+// same it's going
